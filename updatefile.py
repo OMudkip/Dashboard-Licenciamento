45f73ab631e4b273e1bd7d0e961ce6e1de554c84
OMudkip <andre.scavuzzi@gmail.com>
2024-08-12 14:53:54 -0300

    Update updatefile.py

--- updatefile.py
+++ updatefile.py
@@ -2,14 +2,13 @@ import os
 from office365.sharepoint.client_context import ClientContext
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.files.file import File
-local = os.getcwd()
 
 def updatefile():
     user_credentials = UserCredential(os.getenv('LOGIN'),os.getenv('SENHA'))
     ctx = ClientContext('https://engeselt.sharepoint.com/sites/Inovaesdeprocessos/Shared%20Documents/AUTOMAÇÃO%20DE%20PROCESSOS/Dashboard%20Licenciamento/ContratosTotais.xlsx').with_credentials(user_credentials)
 
     file_name = ('ContratosTotais.xlsx')
-    with open(os.path.join(local, file_name), "wb") as local_file:
+    with open(os.path.join(file_name), "wb") as local_file:
         file = (
             File.from_url('https://engeselt.sharepoint.com/sites/Inovaesdeprocessos/Shared%20Documents/AUTOMAÇÃO%20DE%20PROCESSOS/Dashboard%20Licenciamento/ContratosTotais.xlsx')
             .with_credentials(user_credentials)
